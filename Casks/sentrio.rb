b9fbc6d926c9ab51cf9f3de3c4aa53a6b434f067
Yuna <io@yuna.berlin>
2026-08-10 15:17:13 +0200

    ci: automate Homebrew release updates

--- Casks/sentrio.rb
+++ Casks/sentrio.rb
@@ -1,14 +1,16 @@
 cask "sentrio" do
+  # yuna-release: YunaBraska/Sentrio
   version "2026.07.1980531"
+  # yuna-release-asset: Sentrio-{version}.zip
   sha256 "19c058dbd5c75efce4e3f88c31fff85c56ee8b235603895973d1b19f970d0d34"
 
   url "https://github.com/YunaBraska/Sentrio/releases/download/#{version}/Sentrio-#{version}.zip",
       verified: "github.com/YunaBraska/Sentrio/"
   name "Sentrio"
-  desc "macOS menu bar app that auto-switches audio devices"
+  desc "Menu bar app that auto-switches audio devices"
   homepage "https://github.com/YunaBraska/Sentrio"
 
-  depends_on macos: ">= :ventura"
+  depends_on macos: :ventura
 
   app "Sentrio.app"
 end
