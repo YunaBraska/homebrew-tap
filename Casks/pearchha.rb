af14294aff27aa20a7a844f67f037d44f4e88c89
Yuna <io@yuna.berlin>
2026-08-13 17:15:53 +0200

    brew: track PearchHA releases

--- Casks/pearchha.rb
+++ Casks/pearchha.rb
@@ -1,14 +1,16 @@
 cask "pearchha" do
+  # yuna-release: YunaBraska/pearch_ha
   version "2026.7.11"
+  # yuna-release-asset: PearchHA-{version}.zip
   sha256 "5c03d3d5914e063f54c2adbdda0818b405ed57f8bf0512b709f6b2f7bd97a046"
 
   url "https://github.com/YunaBraska/pearch_ha/releases/download/#{version}/PearchHA-#{version}.zip",
       verified: "github.com/YunaBraska/pearch_ha/"
   name "PearchHA"
-  desc "macOS menu bar dashboard for Home Assistant"
+  desc "Menu bar dashboard for Home Assistant"
   homepage "https://github.com/YunaBraska/pearch_ha"
 
-  depends_on macos: ">= :ventura"
+  depends_on macos: :ventura
 
   app "PearchHA.app"
 end
